1d72d070db929bf3794f207675b3e5a9b71a83a5
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-12-13 22:29:06 +0100

    refactor: Refactorizando todo el componente PublicMap y agregando funcionalidades

--- app/Components/PublicMap/PublicMapComponent.tsx
+++ app/Components/PublicMap/PublicMapComponent.tsx
@@ -1,44 +1,141 @@
-import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
-import { MapMouseEvent } from 'mapbox-gl';
-import React, { useState } from 'react';
+import ReactMapboxGl, { Marker, MapContext } from 'react-mapbox-gl';
+import { NavigationControl } from 'mapbox-gl';
+import React, { useState, useEffect, useContext } from 'react';
+import styles from './publicMap.module.scss';
 
-interface MapProps {
-  center: [number, number];
-  zoom: [number];
-  onClick: (map: mapboxgl.Map, event: MapMouseEvent) => void;
-}
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Map = ReactMapboxGl({
   accessToken: 'pk.eyJ1Ijoid3MyMiIsImEiOiJjbHByajZnbHowY3ZxMmpxY3NvdGZuNWtrIn0.JMyy9rU1aVlztcrAxXzALg',
 });
 
-const PublicMap: React.FC = () => {
-  const [center, setCenter] = useState<[number, number]>([-74.01341675982262, 40.71992160719199]);
-  const [zoom, setZoom] = useState<[number]>([5]);
+const NavigationControlWrapper: React.FC = () => {
+  const map = useContext(MapContext);
 
-  const pins = [
-    { coordinates: [-74.01341675982262, 40.71992160719199], color: 'red' }, // Nueva York
-    { coordinates: [-3.7038, 40.4168], color: 'blue' }, // España
-    { coordinates: [127.7669, 35.9078], color: 'green' }, // Corea
-  ];
+  useEffect(() => {
+    if (map) {
+      const navControl = new NavigationControl();
+      map.addControl(navControl, 'top-right');
+    }
+  }, [map]);
 
-  return (
-    <Map
-      style="mapbox://styles/mapbox/light-v10"
-      containerStyle={{
-        height: '80vh',
-        width: '90vw',
-      }}
-      center={center}
-      zoom={zoom}
-    >
-      {pins.map((pin, index) => (
-        <Marker key={index} coordinates={pin.coordinates} anchor="bottom">
-          <div style={{ width: '20px', height: '20px', background: pin.color }} />
-        </Marker>
-      ))}
-    </Map>
-  );
+  return null;
 };
 
-export default PublicMap;
+const PublicMap: React.FC = () => {
+  const [center, setCenter] = useState<[number, number]>([8.8072, 46.8182]);
+  const [zoom, setZoom] = useState<number>(4);
+  const [geoJSONData, setGeoJSONData] = useState<any>({
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.3522, 48.8566], 
+        },
+        properties: {
+          city: 'Paris',
+          color: 'red',
+        },
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-3.7038, 40.4168], 
+        },
+        properties: {
+          city: 'Madrid',
+          color: 'blue',
+        },
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [4.3517, 50.8503], 
+        },
+        properties: {
+          city: 'Brussels',
+          color: 'purple',
+        },
+      },
+    ],
+  });
+
+  const [dynamicMarkers, setDynamicMarkers] = useState<any[]>([]);
+
+  const handleMapClick = (map: mapboxgl.Map, evt: mapboxgl.MapMouseEvent) => {
+    const { lng, lat } = evt.lngLat;
+    console.log('Nueva coordenada:', JSON.stringify([lng, lat]));
+    const newMarker = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [lng, lat],
+      },
+      properties: {
+        city: `Marker ${dynamicMarkers.length + 1}`,
+        color: 'yellow',
+      },
+    };
+
+    setDynamicMarkers((prevMarkers) => [...prevMarkers, newMarker]);
+    setGeoJSONData((prevData: { type: string; features: any[] }) => ({
+      type: 'FeatureCollection',
+      features: [...prevData.features, newMarker],
+    }));
+  };
+
+  const handleRemoveMarker = (index: number) => {
+    const updatedMarkers = [...dynamicMarkers];
+    updatedMarkers.splice(index, 1);
+    setDynamicMarkers(updatedMarkers);
+
+    const updatedGeoJSON = {
+      type: 'FeatureCollection',
+      features: geoJSONData.features.filter((_feature: any, i: number) => i !== index),
+    };
+    setGeoJSONData(updatedGeoJSON);
+  };
+
+
+  return (
+    <div className={styles.mapContainer}>
+      <Map
+        style="mapbox://styles/mapbox/light-v10"
+        containerStyle={{
+          height: '80vh',
+          width: '90vw',
+        }}
+        center={center}
+        zoom={[zoom]}
+        onClick={handleMapClick as any}
+        >
+          <MapContext.Consumer>
+            {() => <NavigationControlWrapper />}
+          </MapContext.Consumer>
+  
+          {geoJSONData && (
+            geoJSONData.features.map((feature: any, index: number) => (
+              <Marker
+              key={index}
+              coordinates={feature.geometry.coordinates}
+              anchor="bottom"
+              className={`${styles.marker} ${styles[feature.properties.color]}`}
+              onClick={() => {
+                console.log('Marcador clickeado:', JSON.stringify(feature.geometry.coordinates));
+                handleRemoveMarker(index);
+              }}
+            >
+                <div className={styles.markerInner} />
+              </Marker>
+            ))
+          )}
+        </Map>
+      </div>
+    );
+  };
+  
+  export default PublicMap;
